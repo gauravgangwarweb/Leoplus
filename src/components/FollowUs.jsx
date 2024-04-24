43eb8bdc84e4e3a6788ec1860fbc405397f2416c
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-04-24 19:56:27 +0530

    Add social media icons to FollowUs component

--- src/components/FollowUs.jsx
+++ src/components/FollowUs.jsx
@@ -1,7 +1,16 @@
 const FollowUs = () => {
     return (
-        <div className="mt-4">
+        <div className="mt-10">
             <h5 className="text-[#103047] text-center text-xl font-semibold uppercase">Follow Us</h5>
+            <div className="flex flex-wrap gap-3 px-8 mt-4 items-center">
+                <img className="w-9 h-9" src="youtube.png" alt="youtube" />
+                <img className="w-9 h-9" src="facebook.png" alt="facebook" />
+                <img className="w-9 h-9" src="x.png" alt="x" />
+                <img className="w-9 h-9" src="telegram.png" alt="telegram" />
+                <img className="w-9 h-9" src="micon.png" alt="micon" />
+                <img className="w-9 h-9" src="discord.png" alt="discord" />
+                <img className="w-9 h-9" src="redit.png" alt="redit" />
+            </div>
         </div>
     );
 };
